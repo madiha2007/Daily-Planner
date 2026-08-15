b48b74b4dd6655632a482f28fde957c11b9fdd61
Madiha Patel <madihapatel20@gmail.com>
2026-08-15 14:43:49 +0530

    Deleted unwanted files 1

--- functions/index.ts
+++
@@ -1,9 +0,0 @@
-import { beforeUserCreated, beforeUserSignedIn } from 'firebase-functions/v2/identity'
-
-export const beforecreated = beforeUserCreated((event) => {
-  return { customClaims: { role: 'authenticated' } }
-})
-
-export const beforesignedin = beforeUserSignedIn((event) => {
-  return { customClaims: { role: 'authenticated' } }
-})
